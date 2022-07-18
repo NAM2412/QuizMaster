51abba8d085284c37248f820b02d893a8c8da577
nguyennamhades <nnhnam2345@gmail.com>
2022-07-18 14:02:17 +0700

    adding array answers

--- Assets/Scripts/QuestionScriptObject.cs
+++ Assets/Scripts/QuestionScriptObject.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="Quiz Question", fileName ="New Question")]
 public class QuestionScriptObject : ScriptableObject
 {
+  static int NumberOfAnswer =4;
   [TextArea(2,6)]
   [SerializeField] string question = "Enter a new question text!";
+  [SerializeField] string [] answers = new string [NumberOfAnswer];
+
+  [SerializeField] int correctAnswerIndex;
+  public string GetQuestion()
+  {
+    return question;
+  }
+  public int GetCorrectAnswerIndex()
+  {
+    return correctAnswerIndex;
+  }
+  public string GetAnswer(int index) 
+  {
+    return answers[index];
+  }
 }
